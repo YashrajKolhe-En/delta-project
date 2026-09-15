d9bc04f6df49746d5a699ef64ed169b9290f0ba6
Yash Kolhe <yashrajkolhe123@gmail.com>
2026-09-16 02:00:49 +0530

    Initial commit

--- controllers/listing.js
+++ controllers/listing.js
@@ -56,10 +56,12 @@ module.exports.createListing = async (req, res, next) => {
 
   let url = req.file.path;
   let filename = req.file.filename;
+
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
-  newListing.image = { url, filename };
 
+
+  newListing.image = { url, filename };
   newListing.geometry = response.body.features[0].geometry;
 
   let savedListing = await newListing.save();
